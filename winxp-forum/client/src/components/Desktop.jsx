424097b1d82e0a1efcd0c16e655dd32c718c11c2
Zach Ludwick <zachludwick@Zachs-MacBook-Air.local>
2025-07-10 23:37:29 +0100

    fixed start.png

--- winxp-forum/client/src/components/Desktop.jsx
+++ winxp-forum/client/src/components/Desktop.jsx
@@ -921,7 +921,7 @@ const Desktop = () => {
           }}
         >
           <img 
-            src="src/assets/start.png" 
+            src="/start.png" 
             alt="Start" 
             style={{
               height: '32px',
